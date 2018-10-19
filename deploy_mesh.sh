3187ab7aa9eb1582fedd619381db55b72f2db4a0
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2018-10-19 14:52:56 -0700

    fork to background process in deploy_mesh.sh
    Signed-off-by: ckno1 <charles.knox@intel.com>

--- deploy_mesh.sh
+++ deploy_mesh.sh
@@ -6,6 +6,6 @@ wget http://www.meshcentral.com/public/dh.ashx?mesh=$1 -O mesh_poky64.msh
 wget http://www.meshcentral.com/public/dh.ashx?agent=18 -O mesh_poky64
 chmod 755 ./mesh_poky64
 ln -s /usr/local/mesh/mesh_poky64 /bin/meshcmd
-./mesh_poky64 start
+su -c "./mesh_poky64 start" mesh &
 # Infinite loop here
 su -c "tail -f /dev/null" mesh
