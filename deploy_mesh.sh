4b0b001f1c1add4b84cf588754c92cc8de9afd39
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2018-10-19 16:23:43 -0700

    still ebugging deploy_mesh.sh
    Signed-off-by: ckno1 <charles.knox@intel.com>

--- deploy_mesh.sh
+++ deploy_mesh.sh
@@ -7,5 +7,5 @@ wget http://www.meshcentral.com/public/dh.ashx?agent=18 -O mesh_poky64
 chmod 755 ./mesh_poky64
 ln -s /usr/local/mesh/mesh_poky64 /bin/meshcmd
 # Infinite loop here
-su -c "tail -f /dev/null" mesh &
-./mesh_poky64 start
+set +e
+./mesh_poky64 start && tail -f /dev/null
