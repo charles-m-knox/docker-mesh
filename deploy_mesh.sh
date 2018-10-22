9913737b251a26dd3ea33f8b37136bcf27138990
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2018-10-22 10:30:06 -0700

    updated comment in deploy_mesh.sh
    Signed-off-by: charles-m-knox <charles.m.knox@gmail.com>

--- deploy_mesh.sh
+++ deploy_mesh.sh
@@ -6,6 +6,6 @@ wget http://www.meshcentral.com/public/dh.ashx?mesh=$1 -O mesh_poky64.msh
 wget http://www.meshcentral.com/public/dh.ashx?agent=18 -O mesh_poky64
 chmod 755 ./mesh_poky64
 ln -s /usr/local/mesh/mesh_poky64 /bin/meshcmd
-# Infinite loop here
 set +e
+# Loops infinitely so that the container stays alive forever
 ./mesh_poky64 start && tail -f /dev/null
